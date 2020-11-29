80ae4709f7a3d261fb78ac4b9c5783f218298c07
kiquetal <kiquetal@gmail.com>
2020-11-28 23:17:15 -0300

    adding app folder

---
+++ app/src/App.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import React from 'react';
+
+import './App.css';
+import { Navbar, NavbarBrand } from 'reactstrap';
+
+class App extends  React.Component {
+  render()
+	{ 
+	return (
+  
+ <div className="App">
+        <Navbar dark color="primary">
+          <div className="container">
+            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+          </div>
+        </Navbar>
+      </div>
+	);
+	}
+}
+
+export default App;

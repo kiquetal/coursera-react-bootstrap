d3ee0f05f1cd69808ea0309c443689b26caf5fda
kiquetal <kiquetal@gmail.com>
2020-12-05 00:31:01 -0300

    adding new files for home component

--- app/src/components/MainComponent.js
+++ app/src/components/MainComponent.js
@@ -3,8 +3,15 @@ import {Navbar, NavbarBrand} from 'reactstrap';
 import Menu from './MenuComponent';
 import DishDetail from './DishdetailComponent';
 import {DISHES} from '../shared/dishes';
+import { LEADERS } from "../shared/leaders";
+import { PROMOTIONS } from "../shared/promotions";
+import { COMMENTS } from "../shared/comments";
 import Header from "./HeaderComponent";
 import Footer  from "./FooterComponent";
+import { Switch, Route, Redirect } from 'react-router-dom';
+import Home from './HomeComponent';
+import Contact from './ContactComponent';
+
 export default class MainComponent extends Component {
   
   constructor(props) {
@@ -12,7 +19,10 @@ export default class MainComponent extends Component {
     
     this.state = {
       dishes: DISHES,
-      selectedDish: null
+      selectedDish: null,
+      comments:COMMENTS,
+      leaders:LEADERS,
+      promotions:PROMOTIONS
     }
   }
   
@@ -24,16 +34,38 @@ export default class MainComponent extends Component {
   
   
   render() {
+  
+    const HomePage = () => {
+      return(
+        <Home
+          dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+          promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
+          leader={this.state.leaders.filter((leader) => leader.featured)[0]}
+        />
+      );
+    }
+    
     
     return (
-      
-      
-      <div>
-        <Header />
-        <Menu dishes={this.state.dishes} onClick={(dishId) => this.onDishSelect(dishId)}/>
-        <DishDetail dish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]}/>
-        <Footer/>
-      </div>
+  <div>
+    <Header/>
+      <Switch>
+        <Route path='/home' component={HomePage} />
+        <Route exact path='/menu' component={() => {
+          return (
+             <React.Fragment>
+            <Menu dishes={this.state.dishes}  onClick={(dishId) => this.onDishSelect(dishId)}/>
+            <DishDetail dish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]}/>
+             </React.Fragment>
+             );
+        }} />
+        <Route exact path='/contactus' component={Contact} />} />
+  
+        <Redirect to="/home" />
+      </Switch>
+    <Footer/>
+  </div>
+
     );
     
     
@@ -41,3 +73,12 @@ export default class MainComponent extends Component {
   
   
 }
+
+/*
+    <div>
+        <Header />
+        <Menu dishes={this.state.dishes} onClick={(dishId) => this.onDishSelect(dishId)}/>
+        <DishDetail dish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]}/>
+        <Footer/>
+      </div>
+ */
